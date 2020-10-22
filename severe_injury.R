bd96655f053e9d05771e3b5b885d5ce176a65955
elaustin <elaustin@uw.edu>
2020-10-21 21:00:32 -0700

    Update severe_injury.R

--- severe_injury.R
+++ severe_injury.R
@@ -5,24 +5,32 @@
 #Version: 1
 ######################################
 
+library(pacman)
+
 #use p_load to install and load new packages
 p_load(data.table)
 p_load(bit64)
 p_load(readxl)
+p_load(table1)
 
-#will only work on local installations of R
-p_load(curl)
-
+#do not run on SPH server
 severeinjuries <- fread("https://www.osha.gov/severeinjury/xml/severeinjury.csv")
 
-naics <- read_excel("Data/2012_NAICS_Structure.xls", 
-                    skip = 2)
+#if on the sph server
+unzip("Data/SevereInjuries20201021.zip", exdir = "Data")
+severeinjuries = fread("Data/SevereInjuries20201021.csv")
+
+#read excel file (skip first 2 rows)
+naics <- read_excel("Data/2012_NAICS_Structure.xls", skip = 2)
+
+#delete first column
 naics$`Change Indicator` = NULL
 
 #create date variable
 p_load(lubridate)
+
 severeinjuries[, EventDate := 
-                 parse_date_time(EventDate, "mdY")]
+                 parse_date_time(EventDate, c("mdY", "Ymd"))]
 severeinjuries[, Year := 
                  year(EventDate)]
 severeinjuries[, Month := 
@@ -32,6 +40,7 @@ severeinjuries[, Month :=
 #create data table
 naics <- setDT(naics)
 
+#order datasets based on column name
 setkey(severeinjuries, "Primary NAICS")
 setkey(naics, "2012 NAICS Code")
 
@@ -75,5 +84,4 @@ leaflet() %>%
   addProviderTiles("Stamen.TonerHybrid") %>%
   addHeatmap(data = severeinjuries_ag,
              lng = ~Longitude, lat = ~Latitude,
-             radius = 5,
-  )
+             radius = 5 )
